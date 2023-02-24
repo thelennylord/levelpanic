00ffa88b53b3eb1ce9ae62c985bd52e4fca05c46
Lenny Lord <32369619+thelennylord@users.noreply.github.com>
2023-02-24 22:06:17 +0530

    Bump version

--- src/main/java/me/thelennylord/levelpanic/LevelPanic.java
+++ src/main/java/me/thelennylord/levelpanic/LevelPanic.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = LevelPanic.MODID, version = LevelPanic.VERSION)
 public class LevelPanic {
     public static final String MODID = "levelpanic";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.3";
 
     public static final Logger logger = LogManager.getLogger(LevelPanic.MODID);
 
